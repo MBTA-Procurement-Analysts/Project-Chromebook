04ab9004250ba6bb430a73d48cdc8095bb2aa066
Rubix <noldakowski@mbta.com>
2018-09-12 15:47:37 -0400

    (Crubix) Commit before deployment, added package.json

---
+++ public/mbta/controllers/dashboard/dashboard.controller.client.js
@@ -0,0 +1,27 @@
+(function () {
+    angular
+        .module('Chrubix')
+        .controller('MBTAlistController', MBTAlistController);
+
+    function MBTAlistController(bidService) {
+        var model = this;
+        model.getBids = function () {
+            bidService
+                .getBids()
+                .then(function (bids) {
+                    console.log(bids);
+                    model.bids = bids;
+                });
+        };
+        model.getBids();
+
+        model.formatDate = function (mongoDate) {
+            var date = new Date(mongoDate);
+            var month = date.getMonth() + 1;
+            var day = date.getDate();
+            var year = date.getFullYear();
+            return month + "/" + day + "/" + year;
+        };
+
+    }
+})();
